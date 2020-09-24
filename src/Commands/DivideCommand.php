60d141f57052161d80ab1ca5063757f5fa115cb0
Rizal Alfarizi <ijal.alfarizi@gmail.com>
2020-09-25 00:01:42 +0700

    add divide number feature

---
+++ src/Commands/DivideCommand.php
@@ -0,0 +1,85 @@
+<?php
+namespace Jakmall\Recruitment\Calculator\Commands;
+
+
+use Illuminate\Console\Command;
+use Jakmall\Recruitment\Calculator\Interfaces\OperatorInterface;
+use Jakmall\Recruitment\Calculator\Commands\BaseCommand;
+
+class DivideCommand extends BaseCommand implements OperatorInterface
+{
+    /**
+     * @var string
+     */
+    protected $signature;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    public function __construct()
+    {
+        $this->setCommandVerb('divide');
+        $this->setCommandPassiveVerb('divided');
+
+        $this->signature = $this->getSignature();
+        $this->description = $this->getDescription();
+
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        $numbers = $this->getInput();
+        $description = $this->generateCalculationDescription($numbers);
+        $result = $this->calculateAll($numbers);
+
+        $this->comment(sprintf('%s = %s', $description, $result));
+    }
+
+    protected function getInput(): array
+    {
+        return $this->argument('numbers');
+    }
+
+    public function generateCalculationDescription(array $numbers): string
+    {
+        $operator = $this->getOperator();
+        $glue = sprintf(' %s ', $operator);
+
+        return implode($glue, $numbers);
+    }
+
+    public function getOperator(): string
+    {
+        return '/';
+    }
+
+    /**
+     * @param array $numbers
+     *
+     * @return float|int
+     */
+    public function calculateAll(array $numbers)
+    {
+        $number = array_pop($numbers);
+
+        if (count($numbers) <= 0) {
+            return $number;
+        }
+
+        return $this->calculate($this->calculateAll($numbers), $number);
+    }
+
+    /**
+     * @param int|float $number1
+     * @param int|float $number2
+     *
+     * @return int|float
+     */
+    public function calculate($number1, $number2)
+    {
+        return $number1 / $number2;
+    }
+}
