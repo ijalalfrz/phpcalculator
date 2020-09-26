e98fb79b172a7fb12ad840713d1bf123590e2fba
Rizal Alfarizi <ijal.alfarizi@gmail.com>
2020-09-27 02:38:55 +0700

    update readme, add try catch in storage service

--- src/Storage/CSVFileStorage.php
+++ src/Storage/CSVFileStorage.php
@@ -23,97 +23,112 @@ class CSVFileStorage implements StorageConnectionInterface, StorageBLLInterface
 
     public function createTable($table_name, $column)
     {
-        $header = [];
-        $header_column = array_keys($column);
-        foreach($header_column as $hc) {
-            array_push($header, $hc);
-        }
+        try {
 
-        $this->csv_path = $this->path.'/'.$table_name.'.csv'; 
-
-        if (!\file_exists($this->csv_path)) {
-            $file = \fopen($this->csv_path, 'w');
-            \fputcsv($file, $header);
-            \fclose($file);
+            $header = [];
+            $header_column = array_keys($column);
+            foreach($header_column as $hc) {
+                array_push($header, $hc);
+            }
+    
+            $this->csv_path = $this->path.'/'.$table_name.'.csv'; 
+    
+            if (!\file_exists($this->csv_path)) {
+                $file = \fopen($this->csv_path, 'w');
+                \fputcsv($file, $header);
+                \fclose($file);
+            }
+        } catch(Exception $e) {
+            throw $e;
         }
 
     }
 
     public function selectAllColumn($table_name)
     {
-        $file = fopen($this->csv_path, 'r');
-        $header = fgetcsv($file, 1024);
-        \fclose($file);
+        try {
 
-        $mapping_idx = [];
-        $idx = 0;
-        foreach ($header as $head) {
-            
-            $mapping_idx[$head] = $idx;
-            $idx ++;
-        }
-
-
-        $data_column = [];
-        $data = [];
-        $idx = 0;
-        $file = fopen($this->csv_path, 'r');
-        while(!feof($file))
-        {
-            $csv_line = fgetcsv($file, 1024);
-
-            if ($idx != 0 && $csv_line) {
+            $file = fopen($this->csv_path, 'r');
+            $header = fgetcsv($file, 1024);
+            \fclose($file);
+    
+            $mapping_idx = [];
+            $idx = 0;
+            foreach ($header as $head) {
                 
-                foreach ($header as $head) {
-                    $data_column[$head] = $csv_line[$mapping_idx[$head]];
-                }
-
-                $data[] = (object) $data_column;
+                $mapping_idx[$head] = $idx;
+                $idx ++;
             }
-
-            $idx++;
+    
+    
+            $data_column = [];
+            $data = [];
+            $idx = 0;
+            $file = fopen($this->csv_path, 'r');
+            while(!feof($file))
+            {
+                $csv_line = fgetcsv($file, 1024);
+    
+                if ($idx != 0 && $csv_line) {
+                    
+                    foreach ($header as $head) {
+                        $data_column[$head] = $csv_line[$mapping_idx[$head]];
+                    }
+    
+                    $data[] = (object) $data_column;
+                }
+    
+                $idx++;
+            }
+            fclose($file);
+    
+            return $data;
+        } catch (Exception $e) {
+            throw $e;
         }
-        fclose($file);
-
-        return $data;
     }
 
     public function filterByColumn($table_name, $column)
     {
-        $file = fopen($this->csv_path, 'r');
-        $header = fgetcsv($file, 1024);
-        \fclose($file);
+        try {
 
-        $mapping_idx = [];
-        $idx = 0;
-        foreach ($header as $head) {
-            $mapping_idx[$head] = $idx;
-            $idx ++;
-        }
-
-        // read data
-        $data = [];
-        $data_column = [];
-        $file = fopen($this->csv_path, 'r');
-        while(!feof($file))
-        {
-            $csv_line = fgetcsv($file, 1024);
-
-            foreach($column as $k => $filter_values) {
-
-                if ($csv_line && \in_array(\lcfirst($csv_line[$mapping_idx[$k]]), $filter_values)) {
-                    foreach ($header as $head) {
-                        $data_column[$head] = $csv_line[$mapping_idx[$head]];
-                    }
-                    $data[] = (object) $data_column;
-                }
+            $file = fopen($this->csv_path, 'r');
+            $header = fgetcsv($file, 1024);
+            \fclose($file);
+    
+            $mapping_idx = [];
+            $idx = 0;
+            foreach ($header as $head) {
+                $mapping_idx[$head] = $idx;
+                $idx ++;
             }
-
-            $idx++;
+    
+            // read data
+            $data = [];
+            $data_column = [];
+            $file = fopen($this->csv_path, 'r');
+            while(!feof($file))
+            {
+                $csv_line = fgetcsv($file, 1024);
+    
+                foreach($column as $k => $filter_values) {
+    
+                    if ($csv_line && \in_array(\lcfirst($csv_line[$mapping_idx[$k]]), $filter_values)) {
+                        foreach ($header as $head) {
+                            $data_column[$head] = $csv_line[$mapping_idx[$head]];
+                        }
+                        $data[] = (object) $data_column;
+                    }
+                }
+    
+                $idx++;
+            }
+            fclose($file);
+            return $data;
+        } catch (Exception $e) {
+            throw $e;
         }
-        fclose($file);
 
-        return $data;
     }
 
     public function deleteAllData($table_name)
--- src/Storage/SQLiteStorage.php
+++ src/Storage/SQLiteStorage.php
@@ -45,48 +45,63 @@ class SQLiteStorage implements StorageConnectionInterface, StorageBLLInterface
 
             $this->conn->exec($sql);
             return TRUE;
-        } catch (Throwable $e) {
+        } catch (\PDOException $e) {
             throw $e;
         }
     }
 
     public function selectAllColumn($table_name)
     {
-        $sql = "SELECT * FROM ".$table_name;
-        $stmt = $this->conn->query($sql);
+        try {
 
-        $data = [];
-        while ($history = $stmt->fetchObject()) {
-            $data[] = $history;
+            $sql = "SELECT * FROM ".$table_name;
+            $stmt = $this->conn->query($sql);
+    
+            $data = [];
+            while ($history = $stmt->fetchObject()) {
+                $data[] = $history;
+            }
+    
+            return $data;
+        } catch (\PDOException $e) {
+            throw $e;
         }
-
-        return $data;
     }
 
     public function getLastInsertedData($table_name)
     {
-        $sql = "SELECT * FROM ".$table_name. " ORDER BY id DESC LIMIT 1";
-        $stmt = $this->conn->query($sql);
+        try {
 
-        while ($history = $stmt->fetchObject()) {
-            $data = $history;
+            $sql = "SELECT * FROM ".$table_name. " ORDER BY id DESC LIMIT 1";
+            $stmt = $this->conn->query($sql);
+    
+            while ($history = $stmt->fetchObject()) {
+                $data = $history;
+            }
+    
+            return $data;
+        } catch (\PDOException $e) {
+            throw $e;
         }
-
-        return $data;
     }
 
     public function filterById($table_name, $id)
     {
-        $sql = "SELECT * FROM ".$table_name. " WHERE id=:id";
-        $stmt = $this->conn->prepare($sql);
+        try {
 
-        $stmt->execute([
-            'id' => $id
-        ]);
-
-        $data = $stmt->fetchObject();        
-
-        return $data;
+            $sql = "SELECT * FROM ".$table_name. " WHERE id=:id";
+            $stmt = $this->conn->prepare($sql);
+    
+            $stmt->execute([
+                'id' => $id
+            ]);
+    
+            $data = $stmt->fetchObject();        
+    
+            return $data;
+        } catch(\PDOException $e) {
+            throw $e;
+        }
     }
 
     public function filterByColumn($table_name, $column)
@@ -129,10 +144,6 @@ class SQLiteStorage implements StorageConnectionInterface, StorageBLLInterface
         } catch (\PDOException $e) {
             throw $e;
         }
-
-
-
-
     }
 
     public function deleteAllData($table_name)
@@ -201,7 +212,7 @@ class SQLiteStorage implements StorageConnectionInterface, StorageBLLInterface
         
     
             return true;
-        } catch(Exception $e) {
+        } catch(\PDOException $e) {
             throw $e;
         }
     }
